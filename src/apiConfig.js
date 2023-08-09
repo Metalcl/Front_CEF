f506fe375f151f45c53770bd6f485a14ad0fe5bb
Metalcl <ojedacasanueva@gmail.com>
2023-08-09 00:26:38 -0400

    Alerts and error handling

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,3 @@
 //URL para cambiar todas las peticiones más rapido
-export const API_URL = 'http://146.83.198.35:1496';
+// export const API_URL = 'http://146.83.198.35:1496';
+export const API_URL = 'http://localhost:4000/api';
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,105 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import { UserDataContext } from '../context/UserDataContext';
 import axios from 'axios';
 import { API_URL } from '../apiConfig';
+import Swal from 'sweetalert2';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Login = () => {
-    sessionStorage.clear();
-
+    const isMounted = useRef(true);
     const { updateUserData } = useContext(UserDataContext);
     const [rut, setRut] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
 
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    useEffect(() => {
+        isMounted.current = true;
+        return () => isMounted.current = false;
+    }, []);
+
+    const saveDataAndProceed = async () => {
+        if (isMounted.current) {
+            setLoading(true);
+        }
         try {
+            if (rut === '' || password === '') {
+                let missingFields = [];
+                if (rut === '') {
+                    missingFields.push('Rut');
+                }
+                if (password === '') {
+                    missingFields.push('Contraseña');
+                }
+                const message = `Los siguientes campos están vacíos: ${missingFields.join(', ')}.`;
+                showErrorAlert(message);
+                return;
+            }
+
             const response = await axios.post(`${API_URL}/user/login`, {
                 rut: rut,
                 password: password,
             });
-            localStorage.setItem('rut', rut);
-            localStorage.setItem('userToken', response.data.token);
-            const token = localStorage.getItem('userToken');
-            const [header, payload, signature] = token.split('.');
-            const decodedPayload = atob(payload);
-            const payloadObject = JSON.parse(decodedPayload);
-            const { apellidos, cargo, nombres, userType, _id } = payloadObject;
-            const userDataObject = { token, apellidos, cargo, nombres, userType, _id };
-            Object.entries(userDataObject).forEach(([key, value]) => {
-                sessionStorage.setItem(key, value);
-            });
-            updateUserData(userDataObject);
-            navigate('/home');
+
+            if (isMounted.current) {
+                const token = response.data.token;
+                localStorage.setItem('rut', rut);
+                localStorage.setItem('userToken', token);
+                const [header, payload, signature] = token.split('.');
+                const decodedPayload = atob(payload);
+                const payloadObject = JSON.parse(decodedPayload);
+                const { apellidos, cargo, nombres, userType, _id } = payloadObject;
+                const userDataObject = { token, apellidos, cargo, nombres, userType, _id };
+                Object.entries(userDataObject).forEach(([key, value]) => {
+                    sessionStorage.setItem(key, value);
+                });
+                updateUserData(userDataObject);
+                setSuccessMessage('Tu sesión ha sido iniciada exitosamente');
+            }
         } catch (error) {
-            console.error('Error en la petición:', error);
+            if (isMounted.current) {
+                showErrorAlert('Los datos ingresados son incorrectos.');
+                console.error('Error en la petición:', error);
+            }
+        } finally {
+            if (isMounted.current) {
+                setLoading(false);
+            }
         }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setErrorMessage('');
+        saveDataAndProceed();
+    };
+
+    useEffect(() => {
+        if (successMessage) {
+            Swal.fire({
+                icon: 'success',
+                title: successMessage,
+                showConfirmButton: false,
+                timer: 1500,
+            });
+
+            setTimeout(() => {
+                navigate('/home');
+            }, 2000);
+        }
+    }, [successMessage]);
+
+    const showErrorAlert = (message) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: message,
+        });
     };
 
     useEffect(() => {
@@ -65,7 +128,6 @@ const Login = () => {
         <div>
             <form onSubmit={handleSubmit}>
                 <h1>Iniciar sesión</h1>
-
                 <label>Rut: </label>
                 <input
                     type="text"
@@ -82,10 +144,12 @@ const Login = () => {
                     value={password}
                 />
                 <br />
-                <button type="submit">Ingresar</button>
+                <button type="submit" disabled={loading}>
+                    {loading ? <CircularProgress size={20} /> : 'Ingresar'}
+                </button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
